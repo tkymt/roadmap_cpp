9a7bfdadb70faed277a58e31dbe4aa4b87be2d49
Taku Yamamoto <jts0ymt@outlook.jp>
2023-12-04 21:06:11 +0900

    BasicOperationsを完了した

---
+++ BasicOperations/main.cpp
@@ -0,0 +1,77 @@
+int main() {
+// éZèpââéZéq
+	// ë´ÇµéZ
+	{
+		int a = 5;
+		int b = 6;
+		int sum = a + b; // òaÇÕ11
+	}
+
+	// à¯Ç´éZ
+	{
+		int a = 10;
+		int b = 6;
+		int diff = a - b; // ç∑ÇÕ4
+	}
+
+	// ä|ÇØéZ
+	{
+		int a = 3;
+		int b = 4;
+		int product = a * b; // êœÇÕ12
+	}
+
+	// äÑÇËéZ
+	{
+		int a = 15;
+		int b = 4;
+		int quotient = a / b; // è§ÇÕ3
+	}
+
+	// ó]ÇËéZ
+	{
+		int a = 15;
+		int b = 4;
+		int remainder = a % b; // ó]ÇËÇÕ3
+	}
+
+// î‰ärââéZéq
+	// ìôâø
+	5 == 5; // true
+	3 == 4; // false
+
+	// ïsìôâø
+	5 != 2; // true
+	1 != 1; // false
+
+	// ëÂÇ»ÇË
+	5 > 3; // true
+	2 > 3; // false
+
+	// è¨Ç»ÇË
+	3 < 5; // true
+	6 < 5; // false
+
+	// à»è„
+	5 >= 5; // true
+	6 >= 2; // true
+	3 >= 4; // false
+
+	// à»â∫
+	4 <= 4; // true
+	2 <= 3; // true
+	5 <= 5; // false
+
+// ò_óùââéZéq
+	// ò_óùêœ
+	true && true; // true
+	true && false; // false
+
+	// ò_óùòa
+	true || true; // true
+	false || false; // false
+
+	// ò_óùî€íË
+	!true; // false
+	!false; // ture
+}
