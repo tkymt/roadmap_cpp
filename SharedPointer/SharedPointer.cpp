ae742691d06a8647f7937335646df9b93f0d39ec
Taku Yamamoto <jts0ymt@outlook.jp>
2023-12-08 17:51:23 +0900

    SharedPointerを完了した

---
+++ SharedPointer/SharedPointer.cpp
@@ -0,0 +1,38 @@
+﻿// SharedPointer.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <memory>
+
+class MyClass {
+public:
+    MyClass() {
+        std::cout << "Constructor is clalled." << std::endl;
+    }
+    ~MyClass() {
+        std::cout << "Destructor is called." << std::endl;
+    }
+};
+
+int main()
+{
+    std::shared_ptr<MyClass> ptr1(new MyClass());
+
+    {
+        std::shared_ptr<MyClass> ptr2 = ptr1;
+
+        std::cout << "Inside the inner scope." << std::endl;
+    }
+    std::cout << "Outside the inner scope." << std::endl;
+}
+
+// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
+// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
+
+// 作業を開始するためのヒント: 
+//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
+//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
+//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
+//   4. エラー一覧ウィンドウを使用してエラーを表示します
+//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
+//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
