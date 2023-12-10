a56b17cee5a914fa1ed61d6fb973e7a1d980a0eb
Taku Yamamoto <jts0ymt@outlook.jp>
2023-12-10 18:01:41 +0900

    ObjectOrientedProgrammingを完了した

--- ObjectOrientedProgramming/ObjectOrientedProgramming.cpp
+++ ObjectOrientedProgramming/ObjectOrientedProgramming.cpp
@@ -3,9 +3,97 @@
 
 #include <iostream>
 
+// クラス
+class Dog {
+public:
+    std::string name;
+    int age;
+
+    void bark() {
+        std::cout << name << " barks!" << std::endl;
+    }
+};
+
+// カプセル化
+// 必要な機能のみを公開すること
+class Dog2 {
+private:
+    std::string name;
+    int age;
+
+public:
+    void setName(std::string n) {
+        name = n;
+    }
+
+    void SetAge(int a) {
+        age = a;
+    }
+
+    void bark() {
+        std::cout << name << " barks!" << std::endl;
+    }
+};
+
+// 継承
+// 既存のクラスから新しいクラスを派生させる機能
+// コードの再利用を可能にする
+class Animal {
+public:
+    void breathe() {
+        std::cout << "I can breathe" << std::endl;
+    }
+};
+
+class Dog3 : public Animal {
+public:
+    void bark() {
+        std::cout << "Dog barks!" << std::endl;
+    }
+};
+
+// ポリモーフィズム
+class Animal2 {
+public:
+    virtual void makeSound() {
+        std::cout << "The Animal makes a sound" << std::endl;
+    }
+};
+
+class Dog4 : public Animal2 {
+public:
+    void makeSound() override {
+        std::cout << "Dog barks!" << std::endl;
+    }
+};
+
+class Cat : public Animal2 {
+public:
+    void makeSound() override {
+        std::cout << "Cat meows!" << std::endl;
+    }
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    {
+        Dog myDog;
+        myDog.name = "Fido";
+        myDog.age = 3;
+        myDog.bark(); // 出力: Fido barks!
+    }
+    {
+        Dog3 myDog;
+        myDog.breathe(); // 出力: I can breathe
+        myDog.bark(); // 出力: Dog barks!
+    }
+    {
+        Animal2* animals[2] = { new Dog4, new Cat};
+        animals[0]->makeSound(); // 出力: Dog barks!
+        animals[1]->makeSound(); // 出力: Cat meows!
+        delete animals[0];
+        delete animals[1];
+    }
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
