8c8be973ad3b70008023c5bd288f403efd5c0f69
Taku Yamamoto <jts0ymt@outlook.jp>
2023-12-11 17:35:24 +0900

    RuleZeroFiveThreeを完了した

--- RuleZeroFiveThree/RuleZeroFiveThree.cpp
+++ RuleZeroFiveThree/RuleZeroFiveThree.cpp
@@ -1,20 +1,86 @@
-﻿// RuleZeroFiveThree.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
-//
+﻿// RuleZeroFiveThree.cpp
 
-#include <iostream>
+#include <string>
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+// ０の法則
+// クラスまたは構造体がリソースを
+// 明示的に管理しない場合、特別な
+// メンバー関数、つまり、デストラ
+// クター、コピー コンストラクター、
+// コピー代入演算子、移動コンスト
+// ラクター、移動代入演算子を定義し
+// てはならない
+struct MyResource {
+    std::string name;
+    int value;
+};
 
-// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
-// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
+// ３の法則
+// リソースを管理するクラスまたは構造
+// 体は、次の 3 つの特別なメンバー関
+// 数を定義する必要があると規定されて
+// います
+// デストラクタ
+// コピーコンストラクタ
+// コピー代入演算子
+class MyResource {
+public:
+    // コンストラクターとデストラクター
+    MyResource(): data(new int[100]) {}
+    ~MyResource() { delete[] data; }
 
-// 作業を開始するためのヒント: 
-//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
-//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
-//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
-//   4. エラー一覧ウィンドウを使用してエラーを表示します
-//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
-//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
+    // コピーコンストラクター
+    MyResource(const MyResource& other) : data(new int[100]) {
+        std::copy(other.data, other.data + 100, data);
+    }
+
+    // 代入演算子のオーバーロード
+    MyResource& operator=(const MyResource& other) {
+        if (&other == this) { return *this; }
+        std::copy(other.data, other.data + 100, data);
+        return *this;
+    }
+
+private:
+    int* data;
+};
+
+// ５の法則
+// ３の法則を拡張して、2 つの特別なメンバー関数を追加します
+// 移動コンストラクター
+// 移動代入演算子
+class MyResource {
+public:
+    // コンストラクターとデストラクター
+    MyResource() : data(new int[100]) {}
+    ~MyResource() { delete[] data; }
+
+    // コピーコンストラクター
+    MyResource(const MyResource& other) : data(new int[100]) {
+        std::copy(other.data, other.data + 100, data);
+    }
+
+    // コピー代入演算子
+    MyResource& operator=(const MyResource& other) {
+        if (&other == this) { return *this; }
+        std::copy(other.data, other.data + 100, data);
+        return *this;
+    }
+
+    // 移動コンストラクター
+    MyResource(MyResource&& other) noexcept : data(other.data) {
+        other.data = nullptr;
+    }
+
+    // 移動代入演算子
+    MyResource& operator=(MyResource&& other) noexcept {
+        if (&other == this) { return *this; }
+        delete[] data;
+        data = other.data;
+        other.data = nullptr;
+        return *this;
+    }
+
+private:
+    int* data;
+};
