d63d17dc5e570b42879648a7e5cf6b228ed0ad5f
Taku Yamamoto <jts0ymt@outlook.jp>
2023-12-11 17:03:44 +0900

    DiamondInheritanceを完了した

--- DiamondInheritance/DiamondInheritance.cpp
+++ DiamondInheritance/DiamondInheritance.cpp
@@ -3,9 +3,41 @@
 
 #include <iostream>
 
+class Base {
+public:
+    void print() {
+        std::cout << "Base class" << std::endl;
+    }
+};
+
+class Derived1 : virtual public Base {
+public:
+    void derived1Print() {
+        std::cout << "Derived1 class" << std::endl;
+    }
+};
+
+class Derived2 : virtual public Base {
+public:
+    void derived2Print() {
+        std::cout << "derived2 class" << std::endl;
+    }
+};
+
+class Derived3 :public Derived1, public Derived2 {
+public:
+    void derived3Print() {
+        std::cout << "Derived3 class" << std::endl;
+    }
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    Derived3 d3;
+    d3.print();
+    d3.derived1Print();
+    d3.derived2Print();
+    d3.derived3Print();
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
