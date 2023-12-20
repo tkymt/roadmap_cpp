ed3d653f0df67b2b2e55ebe1089adee6884c404f
Taku Yamamoto <jts0ymt@outlook.jp>
2023-12-20 16:50:03 +0900

    FullTemplateSpecializationを完了した

--- FullTemplateSpecialization/FullTemplateSpecialization.cpp
+++ FullTemplateSpecialization/FullTemplateSpecialization.cpp
@@ -3,9 +3,30 @@
 
 #include <iostream>
 
+// Generic template
+template<typename T>
+class MyContainer {
+public:
+    void print() {
+        std::cout << "Generic container." << std::endl;
+    }
+};
+
+// 完全なテンプレートの特殊化 for int
+template<>
+class MyContainer<int> {
+public:
+    void print() {
+        std::cout << "Container for integers." << std::endl;
+    }
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    MyContainer<double> d;
+    MyContainer<int> i;
+    d.print(); // Output: Generic container.
+    i.print(); // Output: Conteiner for integers.
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
