525a57960776f52a4cf07d808fd11046ec70bc60
Taku Yamamoto <jts0ymt@outlook.jp>
2023-12-25 16:56:06 +0900

    CuriouslyRecurringTemplatePatternを完了した

--- CuriouslyRecurringTemplatePattern/CuriouslyRecurringTemplatePattern.cpp
+++ CuriouslyRecurringTemplatePattern/CuriouslyRecurringTemplatePattern.cpp
@@ -3,9 +3,36 @@
 
 #include <iostream>
 
+template<typename Derived>
+class Base {
+public:
+    void interface() {
+        static_cast<Derived*>(this)->implementation();
+    }
+
+    void implementation() {
+        std::cout << "Default implementation in Base" << std::endl;
+    }
+};
+
+class Derived1 :public Base<Derived1> {
+public:
+    void implementation() {
+        std::cout << "Custom implementation in Derived1" << std::endl;
+    }
+};
+
+class Derived2 :public Base<Derived2> {
+    // No custom implementation, so Base::implementation will be used.
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    Derived1 d1;
+    d1.interface();
+
+    Derived2 d2;
+    d2.interface();
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
