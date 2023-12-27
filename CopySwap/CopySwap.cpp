8489260a2b5b6fb57739564605dc3db29a383bc8
Taku Yamamoto <jts0ymt@outlook.jp>
2023-12-27 16:55:08 +0900

    CopySwapを完了した

---
+++ CopySwap/CopySwap.cpp
@@ -0,0 +1,40 @@
+﻿// CopySwap.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <utility>
+
+class MyString {
+public:
+    void MySwap(MyString& other) {
+        std::swap(_size,other._size);
+        std::swap(_buffer, other._buffer);
+    }
+
+    MyString& operator=(MyString other) {
+        MySwap(other);
+        return *this;
+    }
+
+private:
+    int _size;
+    int _buffer;
+};
+
+int main()
+{
+    MyString myString1, myString2;
+
+    myString1.MySwap(myString2);
+    myString1 = myString2;
+}
+
+// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
+// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
+
+// 作業を開始するためのヒント: 
+//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
+//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
+//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
+//   4. エラー一覧ウィンドウを使用してエラーを表示します
+//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
+//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
