b280db62aa3009d20fdf1dc235a274563f6859e9
Taku Yamamoto <jts0ymt@outlook.jp>
2023-12-27 16:30:10 +0900

    NonCopyableを完了した

---
+++ NonCopyable/NonCopyable.cpp
@@ -0,0 +1,36 @@
+﻿// NonCopyable.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+class NonCopyable {
+public:
+    NonCopyable() = default;
+    ~NonCopyable() = default;
+
+    // Delete the copy constructor
+    NonCopyable(const NonCopyable&) = delete;
+
+    // Delete the copy assignment operator
+    NonCopyable& operator=(const NonCopyable&) = delete;
+};
+
+class MyClass :private NonCopyable {
+    // MyClass is now non-copyable
+};
+
+int main()
+{
+    MyClass myClass1, myClass2;
+    MyClass myClass3(myClass1); // エラー発生　コピーできない
+    myClass1 = myClass2; // エラー発生　代入できない
+}
+
+// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
+// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
+
+// 作業を開始するためのヒント: 
+//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
+//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
+//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
+//   4. エラー一覧ウィンドウを使用してエラーを表示します
+//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
+//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
